f18f9eab59ec60b31425082c50ada99f13fa40e3
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-06-04 23:37:11 -0400

    modified:   lib/widgets/new_expense.dart

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -8,18 +9,56 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  void _saveTitleInput(String InputValue) {}
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             TextField(
-              onChanged: _saveTitleInput,
+              controller: _titleController,
               maxLength: 50,
-              decoration: InputDecoration(label: Text('Title')),
-            )
+              decoration: const InputDecoration(label: Text('Title')),
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                        controller: _amountController,
+                        maxLength: 15,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          prefixText: '\$',
+                          label: Text('Amount'),
+                        ))),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          log(_titleController.text);
+                          log(_amountController.text);
+                        },
+                        child: const Text('Save Expense'))
+                  ],
+                )
+              ],
+            ),
           ],
         ));
   }
