edb83c0bf780005ae5b715dd0101d29acda92036
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-06-08 23:24:16 -0400

    modified:   lib/widgets/expenses.dart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -44,11 +44,22 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
-    ScaffoldMessenger.of(context)
-        .showSnackBar(const SnackBar(content: Text("Expense Deleted")));
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(seconds: 5),
+      content: const Text("Expense Deleted"),
+      action: SnackBarAction(
+          label: 'undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          }),
+    ));
   }
 
   @override
