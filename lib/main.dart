70b5d1794887e7a1c650b06fa9510177e310c51b
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-06-09 00:01:01 -0400

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 237, 237, 237));
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          // scaffoldBackgroundColor: Color.fromARGB(255, 237, 237, 237)
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: const Color.fromARGB(255, 237, 237, 237),
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
       home: const Expenses(),
     ),
-  );
+  ); //check things you can declare in theme
 }
