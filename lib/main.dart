6f99fc93d0bc8075961a6d58d121f642489ca1a2
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-06-13 23:14:06 -0400

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 33, 32, 32),
+  seedColor: const Color.fromARGB(255, 33, 32, 32),
 );
 
 var kDarkColorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
-  seedColor: Color.fromARGB(255, 37, 35, 35),
+  seedColor: const Color.fromARGB(255, 37, 35, 35),
 );
 
 void main() {
